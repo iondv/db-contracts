530048103fc3507496dd0e65224548cd758b8082
Dan Krasilnikov <krasilneg@yandex.ru>
2021-02-08 23:01:07 +1000

    v2.0.0

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  DataSourceFunctionCodes: require('./lib/DataSourceFunctionCodes'),
+  DataSource: require('./lib/DataSource'),
+  DbSync: require('./lib/DbSync'),
+  SequenceProvider: require('./lib/SequenceProvider')
+};
